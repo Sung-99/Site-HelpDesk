855596da6c6c72ff5a9f475f7c0fa20ef775342f
Sung-99 <vitorhugomota99@hotmail.com>
2023-12-28 11:58:37 -0300

    Initial change

---
+++ jQueryJs/MyJquery.js
@@ -0,0 +1,26 @@
+/*---Show in a slide way my introduction sub divs parents*/
+$(document).ready(function(){
+    $(".intro_themes").click(function(){
+      $(".outer_summ_thems").slideToggle();
+    });
+  });
+/*------------*/
+
+  
+/*----My themes introduction slide when click in introducion*/
+  $(document).ready(function(){
+    $("#click_here_show_TextHardwa").click(function(){
+      $(".Text_div_summ_inline_box_hardwa").slideToggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#click_here_show_TextSoftw").click(function(){
+      $(".Text_div_summ_inline_box_softwa").slideToggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#click_here_show_TextProg").click(function(){
+      $(".Text_div_summ_inline_box_prog").slideToggle();
+    });
+  });
+  /*------*/
